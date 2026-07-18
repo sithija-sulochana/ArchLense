155821d6f89c2d31277a413ae79e3d6d6ba414de
Sithija <your-email@example.com>
2026-07-18 21:29:05 +0530

    added basic structure of this project

--- src/main/java/org/example/archlense/shell/UmlCommands.java
+++ src/main/java/org/example/archlense/shell/UmlCommands.java
@@ -1,4 +1,13 @@
 package org.example.archlense.shell;
 
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
 public class UmlCommands {
+    @ShellMethod(key = "hello", value = "Prints a personalized greeting.")
+    public String sayHello(@ShellOption(defaultValue = "World") String name) {
+        return "👋 Hello, " + name + "!";
+    }
 }
